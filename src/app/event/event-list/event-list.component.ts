fddc994da645d6906d2118cb371f17a3e24b75e6
matekuna92 <kunamate66@gmail.com>
2019-12-08 21:22:00 +0100

    group events with reduce, 3 arrays inside array, 2 ngFor inhtml

--- src/app/event/event-list/event-list.component.ts
+++ src/app/event/event-list/event-list.component.ts
@@ -10,19 +10,47 @@ import { EventModel } from '../../shared/event-model';
 export class EventListComponent implements OnInit {
 
   // nézetben szeretném majd használni az eseményeket, ezért létrehozunk egy lokális verziót is 
-  public events: EventModel[];
+ ///eredeti, a 2 ngFor előtt: public events: EventModel[];
+  public eventsGrouppedBy3: EventModel[];
 
   // EventService-t elkéri a dependency injection-től az angular, az app.module-ban kötöttük be, és az 
   // event.model modellben megírt getAllEvents() metódust hívjuk meg, ami visszaadja az eseményeket
-  constructor(private _eventService: EventService) 
+  constructor(private _eventService: EventService)
   {
+  }
+
+   /* a constructor akkor fut le, mikor létrejön az osztály,
+      az ngOnInit pedig akkor, mikor már a teljes angularos környezet már rendelkezésre áll
+      constructor esetén lehet olyan, hogy a service még nem készült el.. amit az onInit-be rakunk, 
+      az már biztos, hogy létezik, mire ideér az angular */
+
+    
+    ngOnInit() {
       // visszaadja az egyenlőre teszt adattal feltöltött eseményeket
       // event.service.ts -ben van deklarálva a getAllEvents függvény
-      this.events = this._eventService.getAllEvents();
-      console.log(this);
-   }
+      /// eredeti, a 2 ngFor előtt: this.events = this._eventService.getAllEvents();
 
-  ngOnInit() {
+      /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+       initialValue paraméter []: ha nem adunk meg semmit a reduce-nak, akkor alapértelmezetten első futáskor
+      a 0. és 1. elemet fogja venni. Ha megadjuk az utolsó paraméter initialValue-t, akkor az első elem elé
+      ezt beszúrja. Itt egy []-t szúrunk be, ez fogja adni a külső [] keretet a tömbben, amibe a többi
+	  tömb kerül majd 
+
+	   */
+
+	   // cél: [0,1,2,3,4,5,6,7,8] -> [ [0,1,2], [3,4,5], [6,7,8] ]
+
+      this.eventsGrouppedBy3 = this._eventService.getAllEvents()
+      .reduce( (acc, current: EventModel, index: number) => {
+        if (index % 3 === 0)		// 0, 3, és 6 elemeknél 0 a maradék, ezeknél szeretnék új tömböt pusholni a külső tömbbe
+        {
+          acc.push([]);
+        }
+        acc[acc.length - 1].push(current); // aktuális elem tömbbe rakása
+        return acc;
+      }, []);
+
+      console.log(this.eventsGrouppedBy3);
   }
 
 }
